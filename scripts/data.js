c7fa50d351d7d8f0b07f994d13b221c2e56193cb
ItzCrαft <147850863+ItzCraft@users.noreply.github.com>
2026-06-05 23:25:37 +0300

    Add new classes to CLASSES array in data.js

--- scripts/data.js
+++ scripts/data.js
@@ -9,10 +9,30 @@ const CLASSES = [
     "test",
 
     // region blocks
-
     // turrets
     "accel-item-turret","mode-turret","over-heat-turret",
 
     // drills
-    ""
+    "wall-drill",
+
+    // crafting
+    "advanced-consume-generator","chance-crafter","multi-recipe-crafter","advanced-heater-generator",
+
+    // power
+    "adjustable-beam-node","tile-generator",
+
+    // defense
+    "adjustable-shield-wall","restorable-wall",
+
+    // storage
+    "advanced-core-block","generator-core-block",
+
+    // world processor
+    "text-dialog","content-unlocker",
+
+    // other
+    "black-hole-bullet-type","tiled-floor","draw-team",
+
+    // units
+    "overload-ability","effect-weapon"
 ];
